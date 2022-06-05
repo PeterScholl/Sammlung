6ed56473875948b3aa9b81ad49d4ce03062b4d63
Peter Scholl <scholl@unterrichtsportal.org>
2022-06-05 19:32:20 +0200

    closes #5 - only let message disapear if it exists

--- fcc.js
+++ fcc.js
@@ -6,7 +6,9 @@ function init() {
   console.log("fcc-skript initialisieren...");
   var duration = 20000; //20 seconds time for alert to disappear
   //setTimeout(function () { $('#alert').hide(); }, duration);
-  setTimeout(function () { document.getElementById("message_info").style.display = "none"; }, duration);
+  if (document.getElementById("message_info") != null) {
+    setTimeout(function () { document.getElementById("message_info").style.display = "none"; }, duration);
+  }
 }
 
 function loadDocGet(url, cFunction) {
