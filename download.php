78dcdab7be10a22763141f53530abd65cdf5f429
Peter Scholl <scholl@unterrichtsportal.org>
2022-06-08 12:32:46 +0200

    Download window closes automatically on failure - in all cases

--- download.php
+++ download.php
@@ -4,6 +4,7 @@ require_once("config.php");
 require_once("sqlite_inc.php");
 session_start();
 logdb("Session-ID: ".session_id());
+$message_dl = "probably you're not logged in";
 
 if(isset($_REQUEST["file"]) && $_SESSION["user"]>0){
     
@@ -30,34 +31,34 @@ if(isset($_REQUEST["file"]) && $_SESSION["user"]>0){
             readfile($filepath);
             die();
         } else {
-            http_response_code(404);
-	        die();
+            $message_dl="File does not exist";
+            //http_response_code(404);
+            //die();
         }
     } else {
         logdb("Invalid file id! ".$fileid);
-        http_response_code(404);
-        die("Invalid file id! ".$fileid);
+        $message_dl = "Invalid file id!";
+        //http_response_code(404);
+        //die("Invalid file id! ".$fileid);
     }
-} else {
-  //header("Refresh:5; url=".HOMEPAGE);
-  ?>
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <title>Download-Page</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="./fcc.js"></script>
-    <script>window.onload=init();</script>
-  </head>
-  <html>
-    <h1>Could not download file</h1>
-    <p>Probably you're not logged in</p>
-    <Div id="CountDownTime">Closing in 5 seconds</div>
-    <script>CountDownStart(5);</script>
-  </html>
-  <?php
-  logdb("DL: User not logged in?".$_SESSION["user"]);
-  die();  
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Download-Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="./fcc.js"></script>
+  <script>window.onload=init();</script>
+</head>
+<html>
+  <h1>Could not download file</h1>
+  <p><?php echo $message_dl; ?></p>
+  <Div id="CountDownTime">Closing in 6 seconds</div>
+  <script>CountDownStart(5);</script>
+</html>
+<?php
+logdb("DL: User not logged in?".$_SESSION["user"]);
+die();  
 ?>
